f56c40f541d55ce325d6ee43b6dc824837fc7dad
Saber <saber05galedar@gmail.com>
2026-09-21 21:30:47 +0330

    My Session 1, from the Function section

---
+++ function_session1.py
@@ -0,0 +1,12 @@
+# what is function
+# it prevent code duplication ; it helps us when we need to execute a few lines of code many times within the program.
+
+
+# for example a simple function
+def say_hello():
+    print("hello")
+    print("how are you")
+
+
+# if we call it , it execute that command
+say_hello()
